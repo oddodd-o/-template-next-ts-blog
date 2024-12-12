bff89f9a07e4f6b7eea9861536311cc0ebe30cda
odd:odd <eehd80yul@gmail.com>
2024-12-12 16:08:59 +0900

    youtube preview

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
+    images: {
+        domains: ['img.youtube.com'], // YouTube 썸네일 이미지 도메인 추가
+    },
 };
 
 module.exports = nextConfig;
--- src/components/post/BlogPost.tsx
+++ src/components/post/BlogPost.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Card from '../card/Card'
 import Image from 'next/image';
 import { Heart, Zap } from 'lucide-react';
+import { Avatar } from '@chakra-ui/react';
+import { YouTubePreview } from './YouTubePreview';
 
 interface IBlogPostProps {
   isCard?: boolean;
@@ -17,7 +19,11 @@ const BlogPost = ({isCard}: IBlogPostProps) => {
     <Wrapper>
       {/* PostBody */}
       <div className='flex flex-col gap-4'>
-        <Image src="/images/pattern/thumb/blog1.jpeg" alt="아이유" width={500} height={500} className='w-full aspect-video' />
+        <YouTubePreview
+          videoId="xRDKQic90dE" // 실제 YouTube 비디오 ID로 변경
+          title="[조작집🎵] '바람' IU Live Clip (With 윤하)"
+        />
+        {/* <Image src="/images/pattern/thumb/blog1.jpeg" alt="아이유" width={500} height={500} className='w-full aspect-video' /> */}
         <div className='pt-4 pb-8 px-4 flex flex-col gap-2'>
           <strong className='text-xl line-clamp-2'>아이유 예뻐.. 너무너무 예뻐. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nisi porro, in culpa illo, eaque perferendis quaerat molestias voluptatibus natus, delectus similique. Placeat quos, dolorem sint soluta libero cumque quae?</strong>
           <p className='opacity-70 line-clamp-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non expedita dicta, dolor maiores nobis ut quis alias nemo esse est repellendus dolorum sint ratione rem quibusdam doloribus molestiae necessitatibus?</p>
@@ -34,7 +40,7 @@ const BlogPost = ({isCard}: IBlogPostProps) => {
         
         <div className='flex justify-between px-4 py-3'>
           <div className="flex items-center gap-2">
-            <Zap className="w-5 h-5 text-[#FF6B00]" />
+            <Avatar src="/images/pattern/thumb/blog1.jpeg" size='sm' />
             <span>by odada</span>
           </div>
           <div className='flex gap-2'>
